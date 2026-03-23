df382fab6afd28f366832d8121de60c8d3c08d40
Gabriel Chávez <gchavezmardonez@gmail.com>
2026-03-22 21:36:44 -0400

    Add evaluation strategy interface

---
+++ apps/backend/src/modules/submission/strategies/evaluation.strategy.ts
@@ -0,0 +1,32 @@
+import type { Submission } from '../../../generated/prisma/client.js';
+import type { ProgramingLanguage, SubmissionStatus } from '../../../generated/prisma/enums.js';
+
+export interface TestCaseInput {
+  id: string;
+  input: string;
+  expectedOutput: string;
+}
+
+export interface TestCaseResult {
+  testCaseId: string;
+  status: SubmissionStatus;
+  actualOutput: string | null;
+  executionTimeMs: number | null;
+  memoryUsageMb: number | null;
+}
+
+export interface SubmissionExcecutionContext {
+  submissionId: string;
+  sourceCode: string;
+  language: ProgramingLanguage;
+  timeLimitMs: number;
+  memoryLimitMb: number;
+  testCases: TestCaseInput[];
+}
+
+export interface IEvaluationStrategy {
+  readonly language: ProgramingLanguage;
+
+  enqueue(submission: Submission): Promise<string>;
+  execute(context: SubmissionExcecutionContext): Promise<TestCaseResult[]>;
+}
